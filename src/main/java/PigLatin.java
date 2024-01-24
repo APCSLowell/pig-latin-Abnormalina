f8a94ab1a883d505de0a7f7b6f50cddc35c97c64
Alina liu <119249419+Abnormalina@users.noreply.github.com>
2024-01-24 09:49:22 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -29,7 +29,7 @@ public class PigLatin {
     }
     public int findFirstVowel(String sWord) {
  for(int i = 0; i < sWord.length(); i++){
-    if(sWord.charAt('a') || sWord.charAt('e') || sWord.charAt('i') || sWord.charAt('o') || sWord.charAt('u')){  
+       if(word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u'){  
       return i;
     }
   }
