85f02f5ca6e5b1dd36f45cd82dd39daf87ed63ab
Alina liu <119249419+Abnormalina@users.noreply.github.com>
2024-01-24 08:57:21 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -39,7 +39,7 @@ public class PigLatin {
     public String pigLatin(String sWord) {
         //precondition: sWord is a valid String of length greater than 0
         //postcondition: returns the pig latin equivalent of sWord
-       for(i = 0; i < sWord.length(); i++){ 
+       for(int i = 0; i < sWord.length(); i++){ 
 	    if(findFirstVowel(sWord) == -1) {
 		    return sWord + "ay";
 	    }
